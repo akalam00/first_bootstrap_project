e73d5aecd9c3a98ff5fb600a0d46c1ad1e67a44b
ACE User <aceuser@e140-s25.lab.lagcc>
2017-06-14 21:16:12 -0400

    added random gif feature

--- js/script.js
+++ js/script.js
@@ -1,1 +1,9 @@
-script.js
+// var $mainheader = $('#main-header');
+// $mainheader.html('Hello to the world');
+$('#random-gif-btn').click(function(){
+$.get( "http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=american+psycho", function( data ) {
+	var img = '<img src=" ' + data.data.fixed_width_small_url + '">'
+  $ ('#random-gif-container').html(img);	
+});
+
+});
